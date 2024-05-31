3984998f5ae4847813926adda3bd8223ab926903
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-05-31 16:22:33 +0530

    Update forms.py

--- myproject/forms.py
+++ myproject/forms.py
@@ -3,7 +3,7 @@ from .models import Upload  # If using model for uploads
 
 class UploadForm(forms.Form):
     file = forms.FileField(required=True, allow_empty_File=False,
-                            mimetypes=['application/vnd.ms-excel', 'text/csv'])
+                            mimetypes=['application/vnd.ms-excel', 'data/csv'])
     # Add other validation rules as needed
 
     from django import forms
